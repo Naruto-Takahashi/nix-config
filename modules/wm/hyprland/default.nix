b38def8961104a6c395460b2de98fe791d9b0982
Naruto-Takahashi <t.naruto7610@gmail.com>
2026-06-22 19:03:33 +0900

    fix(hyprland): link rofi configs individually to prevent activation conflicts

--- modules/wm/hyprland/default.nix
+++ modules/wm/hyprland/default.nix
@@ -6,7 +6,8 @@
   # -----------------------------------------------------------------------
   xdg.configFile."hypr".source = ./config/hypr;
   xdg.configFile."waybar".source = ./config/waybar;
-  xdg.configFile."rofi".source = ./config/rofi;
+  xdg.configFile."rofi/config.rasi".source = ./config/rofi/config.rasi;
+  xdg.configFile."rofi/colors.rasi".source = ./config/rofi/colors.rasi;
   xdg.configFile."swaync".source = ./config/swaync;
   xdg.configFile."wlogout".source = ./config/wlogout;
   xdg.configFile."cava".source = ./config/cava;
