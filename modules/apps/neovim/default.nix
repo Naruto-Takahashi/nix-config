ed19b8768db3fbb44f6d63514d8d93ed438f4a4c
Naruto-Takahashi <t.naruto7610@gmail.com>
2026-06-24 15:40:00 +0900

    fix(neovim): symlink built-in treesitter parsers on NixOS

--- modules/apps/neovim/default.nix
+++ modules/apps/neovim/default.nix
@@ -1,8 +1,33 @@
-# =========================================================================
-# Neovim テキストエディタ宣言的設定モジュール
-# =========================================================================
 { config, pkgs, ... }:
 
+let
+  # NixOS上でC++等のコンパイルリンクエラーを防ぐため、
+  # Nixpkgsでビルド済みのパーサー群から .so ファイルを抽出し、フラットな1つのディレクトリにまとめてリンクします。
+  treesitter-parsers = pkgs.runCommand "nvim-treesitter-parsers" {} ''
+    mkdir -p $out/parser
+    ${pkgs.lib.concatMapStringsSep "\n" (lang:
+      let
+        pkgName = "tree-sitter-${pkgs.lib.replaceStrings ["_"] ["-"] lang}";
+        parserPkg = pkgs.vimPlugins.nvim-treesitter.builtGrammars.${pkgName};
+      in ''
+        ln -sf ${parserPkg}/parser/${lang}.so $out/parser/
+      ''
+    ) [
+      "rust"
+      "c"
+      "cpp"
+      "lua"
+      "vim"
+      "vimdoc"
+      "query"
+      "python"
+      "javascript"
+      "typescript"
+      "markdown"
+      "markdown_inline"
+    ]}
+  '';
+in
 {
   # Neovim パッケージの有効化
   programs.neovim = {
@@ -19,4 +44,6 @@
   # -----------------------------------------------------------------------
   xdg.configFile."nvim".source = ./nvim;
 
+  # ビルド済みの Treesitter パーサーを Neovim のランタイムパスにシンボリックリンク
+  xdg.dataFile."nvim/site/parser".source = "${treesitter-parsers}/parser";
 }
