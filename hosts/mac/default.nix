6aa00ac26a8daa102fa3c87402be9f5842e6d138
Naruto-Takahashi <t.naruto7610@gmail.com>
2026-07-06 23:33:57 +0900

    fix(mac): add dotfilesPath to mac/default.nix to prevent build failures

--- hosts/mac/default.nix
+++ hosts/mac/default.nix
@@ -26,6 +26,13 @@
   home.homeDirectory = "/Users/nalt";
   home.stateVersion  = "25.11";
 
+  # -----------------------------------------------------------------------
+  # 各モジュールで利用するグローバル引数
+  # -----------------------------------------------------------------------
+  _module.args = {
+    dotfilesPath = "${config.home.homeDirectory}/ghq/github.com/Naruto-Takahashi/nix-config";
+  };
+
   # Home Manager 自体の管理を有効化
   programs.home-manager.enable = true;
 
