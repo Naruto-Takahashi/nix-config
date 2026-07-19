8fda910fd57c9efe55e162219d33fac2a2fb4dbc
Naruto-Takahashi <t.naruto7610@gmail.com>
2026-07-20 01:46:34 +0900

    feat(neovim): add noice.nvim for cmdline/search UI
    Deferred notification handling to snacks.notifier (already enabled)
    to avoid the two plugins fighting over vim.notify.

---
+++ modules/apps/neovim/nvim/lua/plugins/noice.lua
@@ -0,0 +1,26 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {
+    -- 通知(vim.notify)は snacks.nvim の notifier が既に担当しているため、
+    -- noice 側では奪わない (両方が vim.notify を取り合うと二重表示や
+    -- ちらつきの原因になる)。noice はコマンドライン・検索・popupmenu の
+    -- 見た目改善だけに専念させる
+    notify = { enabled = false },
+    lsp = {
+      -- hover/signature も noice に乗せると snacks 由来の他の float と
+      -- 干渉しやすいため、ひとまず素の挙動のままにする
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+        ["vim.lsp.util.stylize_markdown"] = false,
+        ["cmp.entry.get_documentation"] = false,
+      },
+    },
+    presets = {
+      command_palette = true, -- コマンドラインと検索を画面上部にまとめて表示
+      long_message_to_split = true,
+      lsp_doc_border = true,
+    },
+  },
+}
