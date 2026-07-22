5556ff69297dfb5e45e2f81cc3db57fa9cce9f92
Naruto-Takahashi <t.naruto7610@gmail.com>
2026-07-23 03:31:04 +0900

    🐛 fix(neovim): exclude Neo-tree from persistence.nvim sessions

--- modules/apps/neovim/nvim/lua/plugins/persistence.lua
+++ modules/apps/neovim/nvim/lua/plugins/persistence.lua
@@ -3,6 +3,27 @@ return {
   event = "BufReadPre",
   opts = {
   },
+  config = function(_, opts)
+    require("persistence").setup(opts)
+
+    -- persistence.nvimは素の:mksessionを使うため，Neo-treeのサイドバー
+    -- (特殊なスクラッチバッファ)もそのままセッションに巻き込まれてしまう。
+    -- 復元時にNeo-treeの内部状態までは再現されず，空表示になったり，
+    -- heirlineのバッファタブラインに幽霊タブとしてカウントされたりする
+    -- ため，保存前に閉じ，復元後に開き直す
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "PersistenceSavePre",
+      callback = function()
+        pcall(vim.cmd, "Neotree close")
+      end,
+    })
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "PersistenceLoadPost",
+      callback = function()
+        pcall(vim.cmd, "Neotree show")
+      end,
+    })
+  end,
   keys = {
     { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
     { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
